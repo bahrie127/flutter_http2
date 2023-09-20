fa30bc9f62987ddef84b7c8b076caeea4ec0814d
Code with Bahri <codewithbahri@gmail.com>
2023-09-20 11:00:48 +0700

    http2

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,19 @@ import 'package:flutter/material.dart';
 import 'album.dart';
 import 'network_manager.dart';
 
-
-
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   late Future<Album> futureAlbum;
+
+  final TextEditingController _controller = TextEditingController();
 
   @override
   void initState() {
@@ -28,31 +26,41 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter REST API',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-            appBar: AppBar(title: const Text('http example GET')),
-            body: FutureBuilder<Album>(
+      title: 'Flutter REST API',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('http example')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(hintText: 'Enter Title'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  futureAlbum = NetworkManager().createAlbum(_controller.text);
+                });
+              },
+              child: const Text('Create Data'),
+            ),
+            FutureBuilder<Album>(
+              future: futureAlbum,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Center(
-                      child: Column(
-                    children: [
-                      Text(snapshot.data!.title),
-                    ],
-                  ));
+                  return Center(child: Text(snapshot.data!.title));
                 } else if (snapshot.hasError) {
                   return Text('${snapshot.error}');
                 }
                 return const CircularProgressIndicator();
               },
-              future: futureAlbum,
             ),
-          ),
-        );
-
-    
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/network_manager.dart
+++ lib/network_manager.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import 'album.dart';
 
 class NetworkManager {
-
   Future<Album> fetchAlbum() async {
     final response = await http
         .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
@@ -12,6 +11,24 @@ class NetworkManager {
       return Album.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load album');
+    }
+  }
+
+  Future<Album> createAlbum(String title) async {
+    final response = await http.post(
+      Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'title': title,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return Album.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to create album.');
     }
   }
 }
